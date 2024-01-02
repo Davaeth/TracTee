fb662f1d9ca5b0060b57ac52b953c34657046c2c
Patryk Połeć <patryk.polec@snow.dog>
2024-01-02 20:34:22 +0100

    Add basic android setup changes

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -30,16 +30,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation(projects.shared)
+
+    implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
