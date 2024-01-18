a3a6252dbab9b951e4045788f59c91d135e0339d
Patryk Połeć <patryk.polec@snow.dog>
2024-01-18 21:28:40 +0100

    Add cancellable timer

--- androidApp/src/main/java/com/davaeth/tractee/android/ui/TimeDashboard.kt
+++ androidApp/src/main/java/com/davaeth/tractee/android/ui/TimeDashboard.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,13 +17,27 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.davaeth.tractee.utils.toDisplayable
-import java.time.Instant
-import java.util.Date
-import java.util.Timer
-import kotlin.concurrent.timer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
+
+@Stable
+class ComposeTimer {
+    private var running: Boolean = false
+
+    suspend fun schedule(period: Duration = 1.seconds, action: () -> Unit) {
+        running = true
+        while (running) {
+            delay(period)
+            action()
+        }
+    }
+
+    fun stop() {
+        running = false
+    }
+}
 
 @Composable
 
@@ -33,7 +49,8 @@ fun TimeDashboard() {
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 private fun SingleTimer() {
     val currentTime = remember { mutableStateOf(Duration.ZERO) }
-    var currentTimer: Timer? = null
+    val currentTimer: ComposeTimer = remember { ComposeTimer() }
+    val coroutineScope = rememberCoroutineScope()
 
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -47,19 +64,18 @@ private fun SingleTimer() {
 
         Button(
             onClick = {
-                currentTimer = timer(
-                    startAt = Date.from(Instant.now()),
-                    period = 1.seconds.toLong(DurationUnit.MILLISECONDS),
-                    action = { currentTime.value = currentTime.value.plus(1.seconds) })
+                coroutineScope.launch {
+                    currentTimer.schedule(1.seconds) {
+                        currentTime.value = currentTime.value.plus(1.seconds)
+                    }
+                }
             },
         ) {
             Text(text = "Start")
         }
         Button(
             onClick = {
-                currentTimer?.purge()
-                currentTimer?.cancel()
-                currentTimer = null
+                currentTimer.stop()
             },
         ) {
             Text(text = "Stop")
